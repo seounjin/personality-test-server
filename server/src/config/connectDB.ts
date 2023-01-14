c1ecf8e1d49d472aac27b98da4f3e652930238e9
seounjin <invanda7@gmail.com>
2023-01-14 14:41:55 +0900

    CHORE : mongodb connect 코드 수정

--- server/src/config/connectDB.ts
+++ server/src/config/connectDB.ts
@@ -6,7 +6,7 @@ export const initMongoDb = () => {
   mongoose.set("strictQuery", false);
 
   mongoose
-    .connect(`${process.env.MONGODB_URI}`)
+    .connect(process.env.MONGODB_URI ?? '')
     .then(() => console.log("MongoDB conected"))
     .catch((err) => {
       console.log(err);
