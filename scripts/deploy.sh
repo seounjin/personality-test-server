7d249b6a5da6ca7fcf08169674c073e18560440f
seounjin <invanda7@gmail.com>
2023-02-03 15:27:49 +0900

    CICD : ssl 인증서 테스트

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 REPOSITORY=/home/ubuntu/app
 cd $REPOSITORY
 
-echo "Shutting down any running environments"
-docker-compose down
+# echo "Shutting down any running environments"
+# docker-compose down
 
-echo "docker-compose start"
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build
+# echo "docker-compose start"
+# docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build
