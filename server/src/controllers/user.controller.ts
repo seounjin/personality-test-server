e086810d12f28b6a330667f5a873a0d04879374b
seounjin <invanda7@gmail.com>
2023-03-23 22:18:03 +0900

    FEAT : 쿠키 재설정

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -46,7 +46,7 @@ export const userLogin = async (
         httpOnly: true,
         secure: secure,
         sameSite: sameSite,
-        domain: '.app.localhost',
+        // domain: '.app.localhost',
         maxAge: 24 * 60 * 60 * 1000, 
       });
 
@@ -55,7 +55,7 @@ export const userLogin = async (
         httpOnly: true,
         secure: secure,
         sameSite: sameSite,
-        domain: '.app.localhost',
+        // domain: '.app.localhost',
         maxAge: 1000 * 60 * 60 * 24 * 14, 
       });
 
