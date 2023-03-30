c3fbcf240c7c26a84173b45ac22d37d10fda1fa4
seounjin <invanda7@gmail.com>
2023-03-30 17:21:38 +0900

    CHORE : docker compose volumes 추가

--- server/src/controllers/personality.controller.ts
+++ server/src/controllers/personality.controller.ts
@@ -228,7 +228,6 @@ export const setScoreTypeTest = async (
     await saveScoreTypeTest(scoreTypeTest);
     return res.status(201).json({ success: true });    
   } catch (error) {
-    console.log("서버에러", error)
     return res.status(500).json({ success: false }); 
   }
 };
