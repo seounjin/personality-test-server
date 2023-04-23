4a8bc855b10b4f93492050afe791f272a00b1d1a
seounjin <invanda7@gmail.com>
2023-04-23 23:27:25 +0900

    CHORE : aws s3 환경변수 설정

--- server/src/middleware/imageUploader.ts
+++ server/src/middleware/imageUploader.ts
@@ -8,8 +8,8 @@ const allowedMimeTypes = ['image/jpeg', 'image/jpg', 'image/png'];
 const s3Config = new S3Client({
     region: process.env.AWS_REGION,
     credentials:{
-       accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
-       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || ''
+       accessKeyId: process.env.AWS_S3_IMAGE_ACCESS_KEY_ID || '',
+       secretAccessKey: process.env.AWS_S3_IMAGE_SECRET_ACCESS_KEY || ''
    }
  })
 
--- server/src/utils/s3Utils.ts
+++ server/src/utils/s3Utils.ts
@@ -4,8 +4,8 @@ import { IMAGE_DOMAIN } from '../const';
 const s3Config = new S3Client({
     region: process.env.AWS_REGION,
     credentials:{
-       accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
-       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || ''
+       accessKeyId: process.env.AWS_S3_IMAGE_ACCESS_KEY_ID || '',
+       secretAccessKey: process.env.AWS_S3_IMAGE_SECRET_ACCESS_KEY || ''
    }
  })
 
